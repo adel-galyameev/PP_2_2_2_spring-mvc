5eafc0b04b48367e22936fd423e26f0620cc3760
adel-galyameev <112763316+adel-galyameev@users.noreply.github.com>
2022-12-27 08:58:09 +0300

    v1.0

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +14,20 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    private CarService service;
+
     @GetMapping(value = "/cars")
     public String printCar (Model modelMap, @RequestParam(value = "count", required = false,defaultValue = "5") int count) {
         if (count > 5) {
             count = 5;
         }
-        CarService service = new CarServiseImpl();
         List<Car> cars = service.getCarsList(count);
         modelMap.addAttribute("cars",cars);
         return "cars";
 
+    }
+    @Autowired
+    public void setCarService (CarService service) {
+        this.service = service;
     }
 }
--- src/main/java/web/service/CarServiseImpl.java
+++ src/main/java/web/service/CarServiseImpl.java
@@ -1,20 +1,25 @@
 package web.service;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CarServiseImpl  implements CarService{
-    @Override
-    public List<Car> getCarsList(int a) {
-        List<Car> cars = new ArrayList<>();
+
+    private List<Car> cars = new ArrayList<>();
+    {
         cars.add(new Car("Camry","Black", 953));
         cars.add(new Car("Polo", "White",777));
         cars.add(new Car("Actavia","Blue", 326));
         cars.add(new Car("Rapid", "Green", 699));
         cars.add(new Car("Vesta", "Red", 529));
+    }
+    @Override
+    public List<Car> getCarsList(int a) {
         return cars.stream().limit(a).collect(Collectors.toList());
     }
 }
